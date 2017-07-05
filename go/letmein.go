d734ff9523f241447f47eb27feace769530853dc
WindomZ <git.windomz@gmail.com>
2017-07-06 01:28:39 +0800

    feat: implemented by go

--- go/letmein.go
+++ go/letmein.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	_http "github.com/WindomZ/go-develop-kit/http"
 	_path "github.com/WindomZ/go-develop-kit/path"
 	"path"
+	"strings"
 )
 
-func initPath() error {
-	if home, err := _path.HomeDir(); err != nil {
-		return err
-	} else if err := _path.Ensure(path.Join(home,
-		".ssh", "authorized_keys"), false); err != nil {
-		return err
-	}
-	return nil
-}
+const _URL = "https://raw.githubusercontent.com/WindomZ/letmein/master/public_keys"
 
 func main() {
-	if err := initPath(); err != nil {
+	if home, err := _path.HomeDir(); err != nil {
+		panic(err)
+	} else if filePath := path.Join(home, ".ssh", "authorized_keys"); len(filePath) == 0 {
+	} else if client := _http.NewHttpClient(); client == nil {
+	} else if resp, err := client.Get(_URL); err != nil {
+		panic(err)
+	} else if data, err := _http.StringResponseBody(resp); err != nil {
+		panic(err)
+	} else if err := _path.Ensure(filePath, false); err != nil {
+		panic(err)
+	} else if content, err := _path.ReadFile(filePath); err != nil {
+		panic(err)
+	} else if strings.Contains(content, data) {
+	} else if err = _path.AppendToFile(filePath, "", data); err != nil {
 		panic(err)
 	}
 }
